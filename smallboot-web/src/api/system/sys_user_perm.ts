84acf87ae73870c1797ccd75cf2aa2edca0b4e88
zhengqingya <960869719@qq.com>
2023-03-30 18:01:37 +0800

    perf 优化权限

--- smallboot-web/src/api/system/sys_user_perm.ts
+++ smallboot-web/src/api/system/sys_user_perm.ts
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 import { AxiosPromise } from 'axios'
 import { UserInfo } from '@/types/api/system/user'
 
-const BASE_API = '/web/api/system/user'
+const BASE_API = '/web/api/system/perm'
 
 export function getUserPerm(): AxiosPromise<UserInfo> {
   return request({
