ae9bfc5efb49484e4b8fadec53fff355bb31f269
zhengqingya <960869719@qq.com>
2023-08-08 15:48:37 +0800

    perf code

--- smallboot-api/common/web/src/main/java/com/zhengqing/common/web/config/CorsConfig.java
+++ smallboot-api/common/web/src/main/java/com/zhengqing/common/web/config/CorsConfig.java
@@ -6,10 +6,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
+
 /**
- * <p>
- * 全局配置解决跨域
- * </p>
+ * <p> 全局配置解决跨域 </p>
  *
  * @author zhengqingya
  * @description
@@ -20,20 +20,17 @@ public class CorsConfig {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfig = new CorsConfiguration();
-        // 设置你要允许的网站域名，如果全允许则设为 *
-//        corsConfig.addAllowedOrigin("*");
-//        corsConfig.addAllowedOrigin("http://www.zhengqingya.com");
-        // 当allowCredentials为true时，allowedOrigins不能包含特殊值"*"，因为它不能在"Access-Control-Allow-Origin"响应头中设置。
-        // 要允许凭证指向一组起源，可以显式地列出它们，考虑使用“allowedOriginPatterns”代替。
-        corsConfig.addAllowedOriginPattern("*");
-        // 如果要限制 HEADER 或 METHOD 请自行更改
-        corsConfig.addAllowedHeader("*");
-        // 设置允许的方法
-        corsConfig.addAllowedMethod("*");
-        // 是否允许证书
+        // 允许任何来源
+        corsConfig.setAllowedOriginPatterns(Collections.singletonList(CorsConfiguration.ALL));
+        // 允许任何请求头
+        corsConfig.addAllowedHeader(CorsConfiguration.ALL);
+        // 允许任何方法
+        corsConfig.addAllowedMethod(CorsConfiguration.ALL);
+        // 允许凭证
         corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         // 设置允许跨域请求的路由
         source.registerCorsConfiguration("/**", corsConfig);
         return new CorsWebFilter(source);
