ff59adf4f10f2519cd19b2d5563aba4559b0e693
zhengqingya <960869719@qq.com>
2023-09-05 11:12:03 +0800

    style [web] index.scss

--- small-web/src/views/test/index.vue
+++ small-web/src/views/test/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="bg-color-primary h100 w100 flex">
+  <div class="app bg-color-primary h100 w100 flex">
     <div class="bg-color-warning w-200">
       <h1>{{ $filters.sexName(1) }}</h1>
     </div>
 
     <div class="flex-1">
       <div class="bg-color-success font-size-100">123456789123456789</div>
-      <span class="font-size-lg w-200 h-100 line-height-100 text-center bg-color-red display-block">hello</span>
+      <span class="hello font-size-lg w-200 h-100 line-height-100 text-center bg-color-red display-block">hello</span>
       <p v-for="index in 10" :key="index" class="font-size-30">{{ index }}</p>
     </div>
   </div>
@@ -25,3 +25,10 @@ async function handleClick() {
   proxy.submitFail('操作失败');
 }
 </script>
+<style lang="scss" scoped>
+.app {
+  .hello {
+    border: 10px solid #000;
+  }
+}
+</style>
