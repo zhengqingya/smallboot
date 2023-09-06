d9b6618876da144c4b144f581975603cd6917976
zhengqingya <960869719@qq.com>
2023-09-06 16:37:50 +0800

    feat [app] uview-plus

--- small-app/src/config.js
+++ small-app/src/config.js
@@ -1,6 +1,6 @@
 const dev = {
-  baseUrl: 'http://127.0.0.1:888',
-  // baseUrl: 'http://ndebb4.natappfree.cc',
+  // baseUrl: 'http://127.0.0.1:888',
+  baseUrl: 'http://ndebb4.natappfree.cc',
   // baseUrl: "http://172.16.16.88:888",
 };
 
