e93c006ee1755ca8bcb339ebeb1dbbd2e8d1fb72
zhengqingya <960869719@qq.com>
2023-09-28 02:51:34 +0000

    删除文件 small-web/src/components/base

--- small-web/src/components/base/BaseNoData.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex-center-center h-full font-size-base text-color-grey">
-    <slot>暂无数据</slot>
-  </div>
-</template>
-<style lang="scss" scoped></style>
--- small-web/src/components/base/BaseWrapper.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="p-10 h-full">
-    <slot />
-  </div>
-</template>
-<script setup></script>
-<style lang="scss" scoped></style>
