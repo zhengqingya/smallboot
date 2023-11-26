aafa344034e245dd449d6b5a718ce10ffa646ba2
zhengqingya <960869719@qq.com>
2023-11-26 15:32:15 +0800

    feat MyMetaObjectHandler

--- smallboot-api/common/db/src/main/java/com/zhengqing/common/db/config/mybatis/MyMetaObjectHandler.java
+++ smallboot-api/common/db/src/main/java/com/zhengqing/common/db/config/mybatis/MyMetaObjectHandler.java
@@ -34,19 +34,19 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
 
         // 判断对象中是否存在该参数，如果存在则插入数据
         if (metaObject.hasGetter(MybatisConstant.IS_DELETED)) {
-            this.setFieldValByName(MybatisConstant.IS_DELETED, false, metaObject);
+            this.setFiledValue(MybatisConstant.IS_DELETED, false, metaObject);
         }
         if (metaObject.hasGetter(MybatisConstant.CREATE_BY)) {
-            this.setFieldValByName(MybatisConstant.CREATE_BY, userId, metaObject);
+            this.setFiledValue(MybatisConstant.CREATE_BY, userId, metaObject);
         }
         if (metaObject.hasGetter(MybatisConstant.CREATE_TIME)) {
-            this.setFieldValByName(MybatisConstant.CREATE_TIME, nowDate, metaObject);
+            this.setFiledValue(MybatisConstant.CREATE_TIME, nowDate, metaObject);
         }
         if (metaObject.hasGetter(MybatisConstant.UPDATE_BY)) {
-            this.setFieldValByName(MybatisConstant.UPDATE_BY, userId, metaObject);
+            this.setFiledValue(MybatisConstant.UPDATE_BY, userId, metaObject);
         }
         if (metaObject.hasGetter(MybatisConstant.UPDATE_TIME)) {
-            this.setFieldValByName(MybatisConstant.UPDATE_TIME, nowDate, metaObject);
+            this.setFiledValue(MybatisConstant.UPDATE_TIME, nowDate, metaObject);
         }
     }
 
@@ -56,11 +56,20 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void updateFill(MetaObject metaObject) {
         if (metaObject.hasGetter(MybatisConstant.UPDATE_BY)) {
-            this.setFieldValByName(MybatisConstant.UPDATE_BY, Long.valueOf(JwtUserContext.getUserId()), metaObject);
+            this.setFiledValue(MybatisConstant.UPDATE_BY, Long.valueOf(JwtUserContext.getUserId()), metaObject);
         }
         if (metaObject.hasGetter(MybatisConstant.UPDATE_TIME)) {
-            this.setFieldValByName(MybatisConstant.UPDATE_TIME, new Date(), metaObject);
+            this.setFiledValue(MybatisConstant.UPDATE_TIME, new Date(), metaObject);
         }
+    }
+
+
+    private void setFiledValue(String fieldName, Object fieldVal, MetaObject metaObject) {
+        Object value = this.getFieldValByName(fieldName, metaObject);
+        if (value != null) {
+            return;
+        }
+        this.setFieldValByName(fieldName, fieldVal, metaObject);
     }
 
 }
