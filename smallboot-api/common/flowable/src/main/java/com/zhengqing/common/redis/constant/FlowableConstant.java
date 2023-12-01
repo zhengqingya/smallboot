aa58269ef6e4e382517ca4daff99f797362e1702
zhengqingya <960869719@qq.com>
2023-12-01 14:56:24 +0800

    feat 整合flowable

---
+++ smallboot-api/common/flowable/src/main/java/com/zhengqing/common/redis/constant/FlowableConstant.java
@@ -0,0 +1,14 @@
+package com.zhengqing.common.redis.constant;
+
+/**
+ * <p>
+ * 全局常用变量 - Redis缓存
+ * </p>
+ *
+ * @author zhengqingya
+ * @description
+ * @date 2019/10/12 14:47
+ */
+public interface FlowableConstant {
+
+}
