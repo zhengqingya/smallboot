464eec5b8344b468cad65d8d99048c8e0f680d04
zhengqingya <960869719@qq.com>
2025-03-01 19:59:31 +0800

    fix unplugin-vue-reactivity-transform 兼容性问题

--- small-web/vite.config.js
+++ small-web/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 import AutoImport from 'unplugin-auto-import/vite';
-import ReactivityTransform from '@vue-macros/reactivity-transform/vite';
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
