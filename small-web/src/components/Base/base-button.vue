c0c53dde2d815f37d9ed9d4f6001fe48d7f1e813
zhengqingya <960869719@qq.com>
2025-04-02 20:24:59 +0800

    feat base-button.vue

---
+++ small-web/src/components/Base/base-button.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-button style="color: #fff" v-bind="$attrs" type="primary"> <slot /></el-button>
+</template>
+<script setup>
+const { proxy } = getCurrentInstance();
+const props = defineProps({
+  // label: { type: String, default: '' },
+});
+</script>
+<style lang="scss" scoped></style>
